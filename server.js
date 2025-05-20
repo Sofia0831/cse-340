1eecb2a9e59d8b14c0ac93417c216c05ffc27e59
sofiapantas <sofiasarabia0831@gmail.com>
2025-05-20 22:56:23 +0800

    classification view activity

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const static = require("./routes/static")
 
 const baseController = require("./controllers/baseController")
 
+const inventoryRoute = require("./routes/inventoryRoute")
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -29,9 +31,9 @@ app.use(static)
 
 // Index route
 app.get("/", baseController.buildHome) 
-// app.get("/", function(req, res) {
-//   res.render("index", {title: "Home"})
-// })
+
+//Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
