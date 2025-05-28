ae00fdf7ad3ab5e58607f70f2ecb07cc18263ef9
sofiapantas <sofiasarabia0831@gmail.com>
2025-05-28 20:40:52 +0800

    week 4 activities (no css yet)

---
+++ routes/accountRoute.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities")
+const accController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
+//Route to My Account login
+router.get("/login", utilities.handleErrors(accController.buildLogin));
+
+//Route to Registration
+router.get("/register", utilities.handleErrors(accController.buildRegister));
+
+//Registration 
+router.post(
+    "/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accController.registerAccount));
+
+module.exports = router
--- server.js
+++ server.js
@@ -16,7 +16,39 @@ const baseController = require("./controllers/baseController")
 
 const inventoryRoute = require("./routes/inventoryRoute")
 
+const accountRoute = require("./routes/accountRoute")
+
 const utilities = require("./utilities/")
+
+const session = require("express-session")
+const pool = require('./database/')
+
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 /* ***********************
  * View Engine and Templates
@@ -39,6 +71,10 @@ app.get("/trigger-error", utilities.handleErrors(baseController.triggerError));
 
 //Inventory routes
 app.use("/inv", inventoryRoute)
+
+//Account Route
+app.use("/account", accountRoute)
+
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
